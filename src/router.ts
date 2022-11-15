d27ef3b7c458b93fafd4cb0c545f8038a48a46f9
MussaKhan74 <MarkeLoof@gmail.com>
2022-11-15 12:58:36 +0500

    added updates handlers

--- src/router.ts
+++ src/router.ts
@@ -1,31 +1,72 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import {
+  createProduct,
+  deleteProduct,
+  getOneProduct,
+  getProducts,
+  updateProduct,
+} from "./handlers/product";
+import { handleInputError } from "./modules/middleware";
 
 const router = Router();
 
 // Product
 
-router.get("/product", (req, res) => {
-  res.json({ message: "hello!" });
-});
-router.get("/product:id", () => {});
-router.put("/product:id", () => {});
-router.delete("/product:id", () => {});
-router.post("/product", () => {});
+router.get("/product", getProducts);
+router.get("/product/:id", getOneProduct);
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  handleInputError,
+  updateProduct
+);
+router.post(
+  "/product",
+  body("name").isString(),
+  handleInputError,
+  createProduct
+);
+router.delete("/product/:id", deleteProduct);
 
 // Update
 
 router.get("/update", () => {});
-router.get("/update:id", () => {});
-router.put("/update:id", () => {});
-router.delete("/update:id", () => {});
-router.post("/update", () => {});
+router.get("/update/:id", () => {});
+router.put(
+  "/update/:id",
+  body("title").optional(),
+  body("body").optional(),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]).optional(),
+  body("version").optional(),
+  () => {}
+);
+router.post(
+  "/update",
+  body("title").exists().isString(),
+  body("body").exists().isString(),
+  body("productId").exists().isString(),
+  () => {}
+);
+router.delete("/update/:id", () => {});
 
 // Update Point
 
 router.get("/updatepoint", () => {});
-router.get("/updatepoint:id", () => {});
-router.put("/updatepoint:id", () => {});
-router.delete("/updatepoint:id", () => {});
-router.post("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.put(
+  "/updatepoint/:id",
+  body("name").optional().isString(),
+  body("desctption").optional().isString(),
+  () => {}
+);
+router.post(
+  "/updatepoint",
+  body("name").isString(),
+  body("desctption").isString(),
+  body("updatedId").exists().isString(),
+  () => {}
+);
+router.delete("/updatepoint/:id", () => {});
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import router from "./router";
 import morgan from "morgan";
+import cors from "cors";
+import { protect } from "./modules/auth";
+import { createNewUser, signin } from "./handlers/user";
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   console.log("hello from express");
@@ -12,6 +18,8 @@ app.get("/", (req, res) => {
   res.json({ message: "hello" });
 });
 
-app.use("/api", router);
+app.use("/api", protect, router);
+app.post("/user", createNewUser);
+app.post("/signin", signin);
 
 export default app;
